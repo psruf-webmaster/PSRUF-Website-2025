d5325e77c2f404be16a2e74da59aa8cfa17db189
regatti <regatti.nandika@gmail.com>
2025-08-01 15:11:56 -0400

    added template dashboard pages for now

---
+++ client/src/account pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Dashboard() {
+  return <h1>Welcome to the Placeholder Dashboard Page</h1>;
+}
+
+
+//make a unified dashboard for all sisters w events and point values
+//officers will have buttons built on top of dashboard for event creation and management--> new pages?
+// candidates have their own different events and officers on their dashboards
+  //dont need to worry about candidate accounts until spring sem when rush occurs
+      //unless we want those interested in recruiting in fall to have accounts (pnm-- potential new member) and be able to sign up for text reminders
+      //should be able to convert pnm account into a candidate account if we select them as candidates
