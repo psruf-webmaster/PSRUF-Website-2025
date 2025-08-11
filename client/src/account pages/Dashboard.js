1af2e788d28b2d03060abed8ee5f47564c9e5784
regatti <regatti.nandika@gmail.com>
2025-08-11 19:35:28 -0400

    Updates to Dashboard, User.js, server.js, and new admin route

--- client/src/account pages/Dashboard.js
+++ client/src/account pages/Dashboard.js
@@ -5,9 +5,10 @@ export default function Dashboard() {
 }
 
 
-//make a unified dashboard for all sisters w events and point values
+//make a unified dashboard for all sisters w events and point values (candidates have diff events, so they need their own diff dahsboard,
+//alumni also have a comletely diff set of events so they get their own dashbaord??)
 //officers will have buttons built on top of dashboard for event creation and management--> new pages?
-// candidates have their own different events and officers on their dashboards
+// candidates have their own different events and their own class officers  for their dashboards
   //dont need to worry about candidate accounts until spring sem when rush occurs
       //unless we want those interested in recruiting in fall to have accounts (pnm-- potential new member) and be able to sign up for text reminders
       //should be able to convert pnm account into a candidate account if we select them as candidates
