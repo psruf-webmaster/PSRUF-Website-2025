702932d661c3f4c60d3ed1f883e7a03c7b3d32e4
kalischuchhardt <148989348+kalischuchhardt@users.noreply.github.com>
2026-08-23 11:49:25 -0400

    Add new email sign up form

--- client/src/pages/Recruitment.js
+++ client/src/pages/Recruitment.js
@@ -76,7 +76,7 @@ export default function Recruitment() {
               </motion.a>
               <motion.a 
                 className="button-secondary" 
-                href="https://docs.google.com/forms/d/e/1FAIpQLSeTE2XKeGyC-lJQFz9iNIeMwzJIWO0qif_XZ0CYuo3hvKyHmw/viewform"
+                href="https://https://docs.google.com/forms/d/e/1FAIpQLSd7XzAEOR7hOqxU3NOFY9df0ZQFcPGI5LPeSI1pW3jQLo4aXg/viewform"
                 whileHover={{ scale: 1.04, y: -2 }}
                 whileTap={{ scale: 0.96 }}
               >
